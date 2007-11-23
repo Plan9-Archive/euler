621f54af29f0e574008194939963ae1ed4a473d1
glenda <glenda@twilight>
2007-11-22 23:58:24 -0800

    Problem 2

--- 1/1.c
+++ 1/1.c
@@ -2,8 +2,9 @@
 #include <libc.h>
 
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
+23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
@@ -25,6 +26,6 @@ main()
 		}
 	}
 
-	print("Sum of multiples below limit: %d\n", sum);
+	print("Sum: %d\n", sum);
 	exits(0);
 }
